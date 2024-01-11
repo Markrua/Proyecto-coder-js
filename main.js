bb1c401199a16c177cb332045ae1fdd4819140d8
Marcos Rua <ruamarcoseliazar@gmail.com>
2024-01-10 23:25:20 -0300

    se añadio objetos, arrays y funciones de orden superior

--- main.js
+++ main.js
@@ -52,3 +52,68 @@ function funcCamiseta() {
 }
 
 
+
+const camisetas = [];
+
+class Camiseta{
+  constructor(club, colorPri, colorSec){
+    this.id = camisetas.length + 1;
+    this.club = club;
+    this.colorPri = colorPri.toLowerCase();
+    this.colorSec = colorSec.toLowerCase();
+  }
+}
+
+camisetas.push(new Camiseta("River Plate", "Blanco", "Rojo"));
+camisetas.push(new Camiseta("Boca Juniors", "Azul", "Amarillo"));
+camisetas.push(new Camiseta("Independiente", "Rojo", "Blanco"));
+camisetas.push(new Camiseta("Racing Club", "Celeste", "Blanco"));
+camisetas.push(new Camiseta("San Lorenzo", "Azul", "Rojo"));
+camisetas.push(new Camiseta("Estudiantes de la Plata", "Rojo", "Blanco"));
+camisetas.push(new Camiseta("Velez Sarfield", "Blanco", "Azul"));
+camisetas.push(new Camiseta("Newell's Old Boys", "Rojo", "Negro"));
+camisetas.push(new Camiseta("Rosario Central", "Azul", "Amarillo"));
+camisetas.push(new Camiseta("Huracan", "Blanco", "Rojo"));
+camisetas.push(new Camiseta("Lanus", "Granate", "Blanco"));
+camisetas.push(new Camiseta("Banfield", "Blanco", "Verde"));
+camisetas.push(new Camiseta("Argentinos Juniors", "Rojo", "Blanco"));
+camisetas.push(new Camiseta("Platense", "Blanco", "Marron"));
+camisetas.push(new Camiseta("Defensa y Justicia", "Verde", "Amarillo"));
+camisetas.push(new Camiseta("Belgrano", "Celeste", "Blanco"));
+camisetas.push(new Camiseta("Talleres", "Azul", "Blanco"));
+camisetas.push(new Camiseta("Instituto", "Rojo", "Blanco"));
+camisetas.push(new Camiseta("Gimnasia y Esgrima de la Plata", "Blanco", "Azul"));
+camisetas.push(new Camiseta("Godoy Cruz", "Azul", "Blanco"));
+camisetas.push(new Camiseta("Barracas Central", "Rojo", "Blanco"));
+camisetas.push(new Camiseta("Central Cordoba", "Negro", "Blanco"));
+camisetas.push(new Camiseta("Tigre", "Azul", "Rojo"));
+camisetas.push(new Camiseta("Atletico Tucuman", "Blanco", "Celeste"));
+camisetas.push(new Camiseta("Independiente Rivadavia", "Azul", "Blanco"));
+camisetas.push(new Camiseta("Deportivo Riestra", "Negro", "Blanco"));
+camisetas.push(new Camiseta("Union", "Rojo", "Blanco"));
+camisetas.push(new Camiseta("Sarmiento", "Verde", "Blanco"));
+
+
+function filtrarPorColor(){
+  let color = (prompt('Ingrese su color'));
+  let resultado = camisetas.filter((c) => c.colorPri === color || c.colorSec === color);
+  alert(`Equipos que utilizan el color ${color}`)
+  return resultado
+};
+  
+
+alert("Conoce a los equipos de primera division por su color.");
+
+filtrarPorColor().forEach(c =>{
+  alert(`
+  club: ${c.club}
+  colorPri: ${c.colorPri}
+  colorSec: ${c.colorSec}
+  `)
+})
+
+
+
+
+
+
